dc3f3a15786d2745d625300b5bcd472480423d41
johntymuriithi <muriithijohn634@gmail.com>
2024-06-26 21:27:20 +0300

    my first cache system

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,30 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+""" BasicCache module
 """
-This is class will inherit from base caching class
-"""
+
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """ BasicCache is a caching system that inherits from BaseCaching
     """
-    Caching system
-    """
-    def __init__(self):
-        super().__init__()
 
     def put(self, key, item):
+        """ Add an item in the cache
         """
-        method to set the key item in the dict
-        """
-        if key and item is None:
+        if key is None or item is None:
             return
-
         self.cache_data[key] = item
 
     def get(self, key):
+        """ Get an item by key
         """
-        method to get the item per key
-        """
-        if key is None:
-            return
-        return self.cache_data[key]
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
