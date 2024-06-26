b8a22558a96e08b76a42a4faa97fd9f5345d773f
johntymuriithi <muriithijohn634@gmail.com>
2024-06-26 22:38:45 +0300

    my first cache system

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -9,7 +9,7 @@ class LIFOCache(BaseCaching):
     """This class implements the FIFO replacement policy"""
     def __init__(self):
         super().__init__()
-        self.keys_order = []
+        self.last = None
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -17,18 +17,12 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            self.cache_data.popitem()
-        else:
-            self.cache_data[key] = item
-        # if key in self.cache_data:
-        #     self.keys_order.remove(key)
-        # elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-        #     last_key = len(self.keys_order) - 1
-        #     del self.cache_data[last_key]
-        #     print(f"DISCARD: {last_key}")
-        #
-        # self.cache_data[key] = item
-        # self.keys_order.append(key)
+            if self.last:
+                print(f"DISCARD: {self.last}")
+                del self.cache_data[self.last]
+
+        self.cache_data[key] = item
+        self.last = key
 
     def get(self, key):
         """ Get an item by key
